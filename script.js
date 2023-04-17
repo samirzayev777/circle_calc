229e59f3bb039162f5b2c2b4c6a05245f35ba996
samirzayev777 <108356650+samirzayev777@users.noreply.github.com>
2023-04-17 16:22:57 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function calculate() {
+    var a = parseFloat(document.getElementById("a").value);
+    var a = a / 2;
+    var h = parseFloat(document.getElementById("h").value);
+        
+    var r = (a*a + h*h) / (2*h);
+    var cos = (1 - (2*a*a)/(r*r));
+    var rad = Math.acos(cos);
+    var deg = rad * 180 / Math.PI;
+    var l = (Math.PI*r*deg) / 180
+    // var perimeter = 2 * (a + h);
+    // var area = a * h;
+    
+    document.getElementById("result").innerHTML = "R: " + r.toFixed(3) + "<br>α: " + deg.toFixed(3) + "<br>ℓ: " + l.toFixed(3);
+    // document.getElementById("result").innerHTML = "R = " + r.toFixed(3) ;
+  }
+  
